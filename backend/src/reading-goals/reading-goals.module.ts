1f4b7a4676a6c59481bc1cf55448c901cbf25155
narinsak.u <nawongsak@gmail.com>
2026-06-18 09:03:12 +0700

    refactor: remove unnecessary DbModule imports from ReadingGoalsModule and SocialModule

--- backend/src/reading-goals/reading-goals.module.ts
+++ backend/src/reading-goals/reading-goals.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { DbModule } from '../db/db.module';
 import { ReadingGoalsController } from './reading-goals.controller';
 import { ReadingGoalsService } from './reading-goals.service';
 
 @Module({
-  imports: [DbModule],
   controllers: [ReadingGoalsController],
   providers: [ReadingGoalsService],
 })
--- backend/src/social/social.module.ts
+++ backend/src/social/social.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { DbModule } from '../db/db.module';
 import { SocialController } from './social.controller';
 import { SocialService } from './social.service';
 
 @Module({
-  imports: [DbModule],
   controllers: [SocialController],
   providers: [SocialService],
 })
